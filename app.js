7d04563f5b398218cf458f06bdb177d2cd021113
ChrisSensebe <christophe.sensebe@gmail.com>
2015-03-06 22:03:32 +0100

    adding static file server

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var app = require("express")(),
-server  = require('http').createServer(app),
-io      = require("socket.io").listen(server),
-ent     = require("ent"),
-fs      = require("fs"),
-todo    = [];
+var express = require("express"),
+app         = express(),
+server      = require('http').createServer(app),
+io          = require("socket.io").listen(server),
+ent         = require("ent"),
+fs          = require("fs"),
+todo        = [];
 
 //rendu de la page
-app.get("/", function(req, res){
-	res.sendFile(__dirname + "/todo.html");
+app.use(express.static("public"))
+.get("/", function(req, res){
+	res.sendFile(__dirname + "/public/todo.html");
 });
 
 io.sockets.on("connection", function(socket){
---
+++ public/javascript/client.js
@@ -0,0 +1,27 @@
+//connection a socket.io
+var socket = io.connect("http://localhost:8080");
+
+//à la reception de la lsite, rafraichissement de la liste affichée
+socket.on("todo", function(todo){
+	$("#todo").empty();
+	for(var i=0; i<todo.length; i++){
+		$("#todo").append("<li><button>✘</button>" + todo[i] + "</li>");
+	}
+});
+
+//recupération de la tache à ajouter, emission de celle-ci, rajout à la liste affichée
+$("#todoForm").submit(function(){
+	var tache = $("#tache").val();
+	socket.emit("newTask", tache);
+	$("#todo").append("<li><button>✘</button>" + tache + "</li>");
+	$("#tache").val("").focus();
+	return false;
+});
+
+//recupération de la tache à supprimer, émission de celle-ci, supression de la liste affichée
+$("#todo").on("click", "button", function(){
+	$(this).text("");
+	var task = $(this).closest("li").text();
+	socket.emit("deleteTask", task);
+	$(this).closest("li").remove();
+});
