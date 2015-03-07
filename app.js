222e52c3f1f3df9c8125f3cae33fb532776e04b8
ChrisSensebe <christophe.sensebe@gmail.com>
2015-03-07 13:35:51 +0100

    mise en forme code app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ app         = express(),
 server      = require('http').createServer(app),
 io          = require("socket.io").listen(server),
 ent         = require("ent"),
+//todolist
 todo        = [];
 
 //rendu de la page
